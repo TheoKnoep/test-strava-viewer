f550c18580ce883ef3859012edf6e79b9ba964d3
Theo Knoepflin <theo.knoepflin@franceselection.fr>
2023-02-23 11:03:53 +0100

    parser de fichier GPX

--- leaflet-osm/index.js
+++ leaflet-osm/index.js
@@ -1,30 +1,86 @@
 // Initialiser l'affichage de la carte : 
 let mapOptions = {
-    center: [48.8550843,2.3468002835410653], 
-    zoom: 13
+    center: [data[0].lat,data[0].lon], 
+    zoom: 12
 }; 
 
 let map = L.map('the-map', mapOptions); 
 
 
 // On peut choisir parmi de nombreux fonds de carte avec : http://leaflet-extras.github.io/leaflet-providers/preview/ 
-let layer = new L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
-  map.addLayer(layer);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
 
 
   // Créer un nouveau marker : 
-  let marker1 = L.marker([48.8550843,2.3468002835410653]).addTo(map); 
-  marker1.bindPopup("Ici c'est Paris").openPopup(); 
-  let marker2 = L.marker([48.879555100000005,2.3820982131570987]).addTo(map); 
+//   let marker1 = L.marker([48.8550843,2.3468002835410653]).addTo(map); 
+
+
+
+
 
   
 
   // Réagir aux événements sur la carte : 
-  map.on("click", function(e) {
-      new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map); 
-  }); 
+//   map.on("click", function(e) {
+//       new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map); 
+//   }); 
+
+
+
+/* **********************************************************
+ * PARSE XML CONTENT : 
+ */
+
+let pathfile = '../CC_02_23_Paris_Melun_.gpx'; 
+
+displayPointsOnMap(pathfile);
+displayPointsOnMap('../_CC02.gpx', 'green'); 
+
+async function displayPointsOnMap(pathfile, color = 'red') {
+    let data = await parseGPX(pathfile);
+    // tracer les points : 
+    // data.forEach(point => {
+    //     L.marker([point.lat, point.lon]).addTo(map); 
+    // })
+    data.forEach(point => {
+        L.circle([point.lat, point.lon], {
+            color: color, 
+            fillColor: color, 
+            fillOpacity: .5,
+            radius: 10, 
+            title: point.time
+        }).addTo(map); 
+    }
+    )
+}
+
+async function parseGPX(pathfile) {
+    let parser = new DOMParser(); 
+    let course_points = []; 
+    return fetch(pathfile)
+        .then(res => res.text())
+        .then(text => {
+            xmlDoc = parser.parseFromString(text, "text/xml"); 
+            let collection = xmlDoc.querySelectorAll('trkpt'); 
+            collection.forEach(point => {
+                if (!point.querySelector('time')) { throw new Error("Pas d'information d'horaire pour cet itinéraire")}; 
+                course_points.push({
+                    time: point.querySelector('time').textContent, 
+                    lat: point.getAttribute('lat'), 
+                    lon: point.getAttribute('lon')
+                }); 
+            }); 
+            // console.log(course_points); 
+            return course_points; 
+        }); 
+}
+
+
+async function parseTCX(pathtofile) {
+
+}

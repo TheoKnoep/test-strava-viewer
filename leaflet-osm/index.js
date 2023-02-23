ae576ba04d1358194e60418cf1f1f6f7f79fcbaf
Theo Knoepflin <theo.knoepflin@franceselection.fr>
2023-02-23 14:37:48 +0100

    ajout des sliders de trackers au fil du temps

--- leaflet-osm/index.js
+++ leaflet-osm/index.js
@@ -38,12 +38,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let perso = '../CC_02_23_Paris_Melun_.gpx'; 
 const listOfColors = [
-    "red", "green", "blue"
+    "red", "green", "blue", "teal", "crimson", "darkblue", "darkmagenta", "olive", "rebeccapurple", "lightslategray"
 ]; 
+
+
 
 displayPointsOnMap(perso)
 .then(() => {
-    displayPointsOnMap('../CC02.tcx'); 
+    displayPointsOnMap('../premiere.tcx'); 
 })
 
 
@@ -75,8 +77,49 @@ async function displayPointsOnMap(pathfile) {
             fillOpacity: .1,
             radius: 4
         }).addTo(map); 
+    }); 
+
+    // Set time slider : 
+    let ts_start = new Date(data[0].time).getTime(); 
+    let ts_finish = new Date(data[data.length-1].time).getTime(); 
+
+
+    // SET MIN
+    console.log('slider min', document.querySelector('#time').getAttribute('min')); 
+    if (document.querySelector('#time').getAttribute('min') === null) {
+        document.querySelector('#time').setAttribute('min', ts_start); 
+        document.querySelector('#display-time').textContent = new Date(ts_start*1).toLocaleTimeString(); 
+    } else {
+        let min_ts = Math.min(ts_start, document.querySelector('#time').getAttribute('min')*1); 
+        document.querySelector('#time').setAttribute('min', min_ts); 
+        document.querySelector('#display-time').textContent = new Date(min_ts*1).toLocaleTimeString(); 
     }
-    )
+    
+    // SET MAX 
+    if (document.querySelector('#time').getAttribute('max') === null) {
+        document.querySelector('#time').setAttribute('max', ts_finish); 
+    } else {
+        let max_ts = Math.max(ts_finish, document.querySelector('#time').getAttribute('max')*1); 
+        document.querySelector('#time').setAttribute('max', max_ts); 
+    }
+
+    let markers = new Array(); 
+
+    document.querySelector('#time').addEventListener('input', event => {
+        let required_ts = event.target.value*1; 
+        document.querySelector('#display-time').textContent = new Date(required_ts).toLocaleTimeString(); 
+
+        // dislay marker 
+        let positions_of_marker = getPositionForTimeStamp(required_ts, data); 
+
+        if (markers.length === 0) {
+            let tracker = new L.marker([positions_of_marker.lat,positions_of_marker.lon]); 
+            markers.push(tracker)
+            tracker.addTo(map).bindPopup(pathfile.split('/')[pathfile.split('/').length-1]).openPopup(); 
+        } else {
+            markers[0].setLatLng([positions_of_marker.lat,positions_of_marker.lon]); 
+        }
+    }); 
 }
 
 
@@ -102,6 +145,21 @@ async function parseGPX(pathfile) {
 }
 
 
+function getPositionForTimeStamp(timestamp, collection_of_points) {
+    let out = ''; 
+    for (let i = 0; i < collection_of_points.length; i++) {
+        if (new Date(collection_of_points[i].time).getTime() - new Date(timestamp).getTime() > 0) {
+            out = collection_of_points[i]; 
+            break; 
+        }
+    }
+    return out;
+}
+
+
+function displayMarker(lat, lon) {
+
+}
 
 
 parseTCX('../CC02.tcx'); 
